3bc66a736b1ca0a8435726c36ae3b59445782b5c
momohturay <turaysamorie@gmail.com>
2019-12-12 11:32:09 -0500

    Added Controller Support and "Control" Class is able to map controls to actions

--- DBZ Fight/GameScene.swift
+++ DBZ Fight/GameScene.swift
@@ -8,16 +8,21 @@
 
 import SpriteKit
 import GameplayKit
+import GameController
+
+var player = SKSpriteNode()
+var aiFighter = SKSpriteNode()
 
 class GameScene: SKScene {
     
 
-    var Player = SKSpriteNode()
-    var aiFighter = SKSpriteNode()
-    
+
         override func didMove(to view: SKView)
          {
-             
+             spawnSprite()
+            
+             ObserveForGameControllers()
+            
              let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
              borderBody.friction = 0.0
              self.physicsBody = borderBody
@@ -29,6 +34,63 @@ class GameScene: SKScene {
             
              
          }
+    func ObserveForGameControllers ()
+        
+        // func is called to detect any controllers currently connected to the apple tv
+        
+    {
+        NotificationCenter.default.addObserver(self, selector: #selector(connectControllers), name: NSNotification.Name.GCControllerDidConnect, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(disconnectControllers), name: NSNotification.Name.GCControllerDidDisconnect, object: nil)
+    }
+    
+    
+    @objc func connectControllers()
+        // This Function is called when a controller is connected to the Apple TV
+        {
+    //Unpauses the Game if it is currently paused
+    self.isPaused = false
+    //Used to register the Nimbus Controllers to a specific Player Number
+    var indexNumber = 0
+    // Run through each controller currently connected to the system
+    for controller in GCController.controllers() {
+    //Check to see whether it is an extended Game Controller (Such as a Nimbus)
+    if controller.extendedGamepad != nil {
+    controller.playerIndex = GCControllerPlayerIndex.init(rawValue: indexNumber)!
+    indexNumber += 1
+    setupControllerControls(controller: controller)
+            }
+        }
+    }
+    
+    @objc func disconnectControllers()
+    // This Function is called when a controller is disconnected from the Apple TV
+    {
+    // Pauses the Game if a controller is disconnected
+    self.isPaused = true
+    }
+    
+    func setupControllerControls(controller: GCController)
+    {
+    // Checks the controller when anything is moved or pressed on it
+    controller.extendedGamepad?.valueChangedHandler =
+    {
+    (gamepad: GCExtendedGamepad, element: GCControllerElement) in
+    // Add movement in here for sprites of the controllers
+     self.controllerInputDetected(gamepad: gamepad, element: element, index: controller.playerIndex.rawValue)
+        }
+    }
+    
+    
+    func spawnSprite ()
+        //spawns Sprite
+    {
+        player = SKSpriteNode(color: UIColor.orange, size: CGSize(width: 40, height: 40))
+        
+        player.position = CGPoint(x: 1, y: 2)
+    }
+    
+    
+    
     func creataipaddle ()
      {
         //create the paddle at tope of the screen
@@ -44,5 +106,5 @@ class GameScene: SKScene {
         aiFighter
             .physicsBody?.isDynamic = false
         
-}
+    }
 }
