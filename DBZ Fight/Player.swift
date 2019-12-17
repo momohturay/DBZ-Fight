701d9d793d4dd72e4d08921bbf42a698f97e0ac8
AirBarrett <thomasbarrett512001@gmail.com>
2019-12-17 12:18:31 -0500

    player class

---
+++ DBZ Fight/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  DBZ Fight
+//
+//  Created by  on 12/17/19.
+//  Copyright © 2019 Air Barrett Apps. All rights reserved.
+//
+
+import Foundation
+struct Player
+{
+    var health: Int
+    var Armor: Int
+    
+}
---
+++ DBZ Fight/playerhealth.swift
@@ -0,0 +1,19 @@
+//
+//  playerhealth.swift
+//  DBZ Fight
+//
+//  Created by  on 12/17/19.
+//  Copyright © 2019 Air Barrett Apps. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+
+
+
+
+
+
